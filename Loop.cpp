78aa7e55aa7567a2a8d559f5396990d119f2400d
Phruek <33310@rb.ac.th>
2022-12-17 18:37:41 +0700

    file

---
+++ Loop.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int num;
+	for (int num; num<=100; num++)
+	{
+	printf(" %d ",num);
+	}
+}
---
+++ บวกเลข.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+main()
+{
+    int a, b, total;
+
+    a = 
+    b =  
+    total = a + b;
+    
+    printf( "%d + %d = %d", a, b, total );
+}
